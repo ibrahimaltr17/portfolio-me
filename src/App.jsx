36d700769516f6dc360aa2bd0b5c85a716ac336a
ibrahimaltr17 <ibrahimaltr17@gmail.com>
2025-10-16 22:58:59 +0600

    Navbar Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import background from './assets/macOS.jpg';
+import background from './assets/macOS2.jpg';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
@@ -9,7 +10,8 @@ const App = () => {
     >
       <div className="absolute inset-0 backdrop-blur-lg bg-black/40"></div>
       <div className="relative flex items-center justify-center h-full p-5">
-        <div className="backdrop-blur-lg bg-white/10 border border-white/20 rounded-2xl p-10 text-center w-full h-full">
+        <div className="backdrop-blur-lg bg-white/10 border border-white/20 rounded-3xl p-10 text-center w-full h-full">
+        <Navbar></Navbar>
           <h1 className="text-white text-5xl font-bold mb-3">Welcome to My Portfolio</h1>
           <p className="text-gray-200 text-lg">
             Front-end Developer | Clean, Modern, Minimal Design
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { FaCircle } from "react-icons/fa";
+import profile from '../../assets/profile.jpg'
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className='flex justify-between items-center '>
+            <div className='flex gap-3'>
+                <div className='w-5 h-5 text-red-500'>
+                    <FaCircle />
+                </div>
+                <div className='w-5 h-5 text-yellow-500'>
+                    <FaCircle />
+                </div>
+                <div className='w-5 h-5 text-green-500'>
+                    <FaCircle />
+                </div>
+            </div>
+            <div>
+                <div className="avatar">
+                    <div className="ring-primary ring-offset-base-100 w-12 rounded-full ring-2 ring-offset-2">
+                        <img src={profile} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
